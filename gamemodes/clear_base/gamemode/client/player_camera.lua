d6c745ab157a9502ffa1cf25f2d7e2bd5a0f36d9
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-27 17:42:24 +0400

    Full Rewrite

--- gamemodes/clear_base/gamemode/client/player_camera.lua
+++ gamemodes/clear_base/gamemode/client/player_camera.lua
@@ -1,82 +1,98 @@
-local player_manager_RunClass = player_manager.RunClass
-local drive_CalcView = drive.CalcView
-local util_TraceHull = util.TraceHull
-local hook_Run = hook.Run
-local IsValid = IsValid
-local Vector = Vector
-local Angle = Angle
+-- GM:CalcVehicleView
+do
 
-local blockedHullEnts = {
-	["prop_physics"] = true,
-	["prop_dynamic"] = true,
-	["prop_ragdoll"] = true,
-	["phys_bone_follower"] = true,
-}
+	local util_TraceHull = util.TraceHull
 
-function GM:CalcVehicleView(veh, ply, view)
-	if (veh["GetThirdPersonMode"] == nil) or (ply:GetViewEntity() != ply) then
-		return
-	end
-
-	if not veh:GetThirdPersonMode() then
-		return view
-	end
-
-	local mn, mx = veh:GetRenderBounds()
-	local radius = (mn - mx):Length()
-	local radius = radius + radius * veh:GetCameraDistance()
-
-	local TargetOrigin = view["origin"] + (view["angles"]:Forward() * -radius)
 	local WallOffset = 4
-
-	local tr = util_TraceHull({
-		start = view["origin"],
-		endpos = TargetOrigin,
-		filter = function(ent)
-			return blockedHullEnts[ent:GetClass()] or ent:IsVehicle()
-		end,
-		mins = Vector(-WallOffset, -WallOffset, -WallOffset),
-		maxs = Vector(WallOffset, WallOffset, WallOffset),
-	})
-
-	view["origin"] = tr["HitPos"]
-	view["drawviewer"] = true
-
-	if (tr["Hit"] and !tr["StartSolid"]) then
-		view["origin"] = view["origin"] + tr["HitNormal"] * WallOffset
-	end
-
-	return view
-end
-
-function GM:CalcView(ply, origin, angles, fov, znear, zfar)
-	local view = {
-		["origin"] = origin,
-		["angles"] = angles,
-		["fov"] = fov,
-		["znear"] = znear,
-		["zfar"] = zfar,
-		["drawviewer"] = false,
+	local blockedHullEnts = {
+		["prop_physics"] = true,
+		["prop_dynamic"] = true,
+		["prop_ragdoll"] = true,
+		["phys_bone_follower"] = true,
 	}
 
-	local veh = ply:GetVehicle()
-	if IsValid(veh) then
-		return hook_Run("CalcVehicleView", veh, ply, view)
+	local mins = Vector( -WallOffset, -WallOffset, -WallOffset )
+	local maxs = Vector( WallOffset, WallOffset, WallOffset )
+
+	local function filter( ent )
+		return blockedHullEnts[ ent:GetClass() ] or ent:IsVehicle()
 	end
 
-	if drive_CalcView(ply, view) then
+	function GM:CalcVehicleView( veh, ply, view )
+		if (veh.GetThirdPersonMode == nil) or (ply:GetViewEntity() ~= ply) then
+			return
+		end
+
+		if veh:GetThirdPersonMode() then
+			local mn, mx = veh:GetRenderBounds()
+			local radius = (mn - mx):Length()
+
+			local tr = util_TraceHull({
+				["start"] = view.origin,
+				["endpos"] = view.origin + ( view.angles:Forward() * (radius + radius * veh:GetCameraDistance()) * -1 ),
+				["filter"] = filter,
+				["mins"] = mins,
+				["maxs"] = maxs
+			})
+
+			view.origin = tr.HitPos
+			view.drawviewer = true
+
+			if (tr.Hit == true) then
+				if (tr.StartSolid == true) then
+					return view
+				end
+
+				view.origin = view.origin + tr.HitNormal * WallOffset
+			end
+		end
+
 		return view
 	end
 
-	player_manager_RunClass(ply, "CalcView", view)
+end
 
-	local wep = ply:GetActiveWeapon()
-	if IsValid(wep) then
-		if (wep["CalcView"] == nil) then return view end
+-- GM:CalcView
+do
 
-		local origin, angles, fov = wep:CalcView(ply, Vector(view["origin"]), Angle(view["angles"]), view["fov"])
-		view["origin"], view["angles"], view["fov"] = origin or view["origin"], angles or view["angles"], fov or view["fov"]
+	local Angle = Angle
+	local Vector = Vector
+	local IsValid = IsValid
+	local hook_Run = hook.Run
+	local drive_CalcView = drive.CalcView
+	local player_manager_RunClass = player_manager.RunClass
+
+	function GM:CalcView( ply, origin, angles, fov, znear, zfar )
+		local view = {
+			["origin"] = origin,
+			["angles"] = angles,
+			["fov"] = fov,
+			["znear"] = znear,
+			["zfar"] = zfar,
+			["drawviewer"] = false
+		}
+
+		local veh = ply:GetVehicle()
+		if IsValid( veh ) then
+			return hook_Run( "CalcVehicleView", veh, ply, view )
+		end
+
+		if drive_CalcView( ply, view ) then
+			return view
+		end
+
+		player_manager_RunClass( ply, "CalcView", view )
+
+		local wep = ply:GetActiveWeapon()
+		if IsValid( wep ) then
+			if (wep.CalcView == nil) then
+				return view
+			end
+
+			local origin, angles, fov = wep:CalcView( ply, Vector( view.origin ), Angle( view.angles ), view.fov )
+			view.origin, view.angles, view.fov = origin or view.origin, angles or view.angles, fov or view.fov
+		end
+
+		return view
 	end
-
-	return view
 end
