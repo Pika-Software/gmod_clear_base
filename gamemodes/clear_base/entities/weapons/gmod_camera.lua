70d709a5c484cca8b629cb4f20620c695bd52d42
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-27 23:56:28 +0400

    Camera Rewrite

--- gamemodes/clear_base/entities/weapons/gmod_camera.lua
+++ gamemodes/clear_base/entities/weapons/gmod_camera.lua
@@ -1,8 +1,7 @@
-
 AddCSLuaFile()
 
-SWEP.ViewModel = Model("models/weapons/c_arms_animations.mdl")
-SWEP.WorldModel = Model("models/MaxOfS2D/camera.mdl")
+SWEP.ViewModel = Model( "models/weapons/c_arms_animations.mdl" )
+SWEP.WorldModel = Model( "models/MaxOfS2D/camera.mdl" )
 
 SWEP.Primary.ClipSize		= -1
 SWEP.Primary.DefaultClip	= -1
@@ -14,7 +13,6 @@ SWEP.Secondary.DefaultClip	= -1
 SWEP.Secondary.Automatic	= true
 SWEP.Secondary.Ammo			= "none"
 
-
 SWEP.PrintName	= "#GMOD_Camera"
 
 SWEP.Slot		= 5
@@ -24,203 +22,194 @@ SWEP.DrawAmmo		= false
 SWEP.DrawCrosshair	= false
 SWEP.Spawnable		= true
 
-SWEP.ShootSound = Sound("NPC_CScanner.TakePhoto")
+SWEP.HoldType = "camera"
+
+local CLIENT = CLIENT
+local SERVER = SERVER
+local IN_ATTACK2 = IN_ATTACK2
 
 if (SERVER) then
 
 	SWEP.AutoSwitchTo		= false
 	SWEP.AutoSwitchFrom		= false
 
-	--
-	-- A concommand to quickly switch to the camera
-	--
-	concommand.Add("gmod_camera", function(player, command, arguments)
-
-		player:SelectWeapon("gmod_camera")
-
+	concommand.Add("gmod_camera", function( ply, class )
+		if ply:HasWeapon( class ) then
+			ply:SelectWeapon( class )
+		end
 	end)
 
 end
 
---
--- Network/Data Tables
---
 function SWEP:SetupDataTables()
 
-	self:NetworkVar("Float", 0, "Zoom")
-	self:NetworkVar("Float", 1, "Roll")
+	self:NetworkVar( "Float", 0, "Zoom" )
+	self:NetworkVar( "Float", 1, "Roll" )
 
 	if (SERVER) then
-		self:SetZoom(70)
-		self:SetRoll(0)
+		self:SetZoom( 75 )
+		self:SetRoll( 0 )
 	end
 
 end
 
---
--- Initialize Stuff
---
 function SWEP:Initialize()
-
-	self:SetHoldType("camera")
-
+	self:SetHoldType( self.HoldType )
 end
 
---
--- Reload resets the FOV and Roll
---
 function SWEP:Reload()
+	local ply = self:GetOwner()
+	if IsValid( ply ) then
+		if ply:KeyDown( IN_ATTACK2 ) then
+			return
+		end
 
-	if (!self.Owner:KeyDown(IN_ATTACK2)) then self:SetZoom(self.Owner:IsBot() && 75 || self.Owner:GetInfoNum("fov_desired", 75)) end
-	self:SetRoll(0)
-
+		self:SetZoom( ply:IsBot() and 75 or ply:GetInfoNum( "fov_desired", 75 ) )
+		self:SetRoll( 0 )
+	end
 end
 
---
--- PrimaryAttack - make a screenshot
---
 function SWEP:PrimaryAttack()
-
 	self:DoShootEffect()
 
-	-- If we're multiplayer this can be done totally clientside
-	if (!game.SinglePlayer() && SERVER) then return end
-	if (CLIENT && !IsFirstTimePredicted()) then return end
-
-	self.Owner:ConCommand("jpeg")
-
+	if (CLIENT) and IsFirstTimePredicted() then
+		RunConsoleCommand( "jpeg" )
+	end
 end
 
---
--- SecondaryAttack - Nothing. See Tick for zooming.
---
 function SWEP:SecondaryAttack()
 end
 
---
--- Mouse 2 action
---
-function SWEP:Tick()
+do
 
-	if (CLIENT && self.Owner != LocalPlayer()) then return end -- If someone is spectating a player holding this weapon, bail
+	local math_Clamp = math.Clamp
+	local FrameTime = FrameTime
 
-	local cmd = self.Owner:GetCurrentCommand()
-
-	if (!cmd:KeyDown(IN_ATTACK2)) then return end -- Not holding Mouse 2, bail
-
-	self:SetZoom(math.Clamp(self:GetZoom() + cmd:GetMouseY() * FrameTime() * 6.6, 0.1, 175)) -- Handles zooming
-	self:SetRoll(self:GetRoll() + cmd:GetMouseX() * FrameTime() * 1.65) -- Handles rotation
+	function SWEP:Tick()
+		local ply = self:GetOwner()
+		if IsValid( ply ) then
+			if (CLIENT) and (ply:EntIndex() == LocalPlayer()) then
+				local cmd = ply:GetCurrentCommand()
+				if cmd:KeyDown( IN_ATTACK2 ) then
+					self:SetZoom( math_Clamp( self:GetZoom() + cmd:GetMouseY() * FrameTime() * 6.6, 0.1, 175 ) )
+					self:SetRoll( self:GetRoll() + cmd:GetMouseX() * FrameTime() * 1.65 )
+				end
+			end
+		end
+	end
 
 end
 
---
--- Override players Field Of View
---
-function SWEP:TranslateFOV(current_fov)
-
+function SWEP:TranslateFOV()
 	return self:GetZoom()
-
 end
 
---
--- Deploy - Allow lastinv
---
 function SWEP:Deploy()
-
 	return true
-
 end
 
---
--- Set FOV to players desired FOV
---
 function SWEP:Equip()
+	local ply = self:GetOwner()
+	if IsValid( ply ) then
+		if ply:IsBot() then return end
+		if (self:GetZoom() == 70) and ply:IsPlayer() then
+			self:SetZoom( ply:GetInfoNum( "fov_desired", 75 ) )
+		end
+	end
+end
 
-	if (self:GetZoom() == 70 && self.Owner:IsPlayer() && !self.Owner:IsBot()) then
-		self:SetZoom(self.Owner:GetInfoNum("fov_desired", 75))
+function SWEP:ShouldDropOnDie()
+	return false
+end
+
+do
+
+	local PLAYER_ATTACK1 = PLAYER_ATTACK1
+	local ACT_VM_PRIMARYATTACK = ACT_VM_PRIMARYATTACK
+
+	SWEP.ShootSound = Sound( "NPC_CScanner.TakePhoto" )
+
+	function SWEP:DoShootEffect()
+
+		self:EmitSound( self.ShootSound )
+		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+
+		local ply = self:GetOwner()
+		if IsValid( ply ) then
+			ply:SetAnimation( PLAYER_ATTACK1 )
+
+			if (SERVER) then
+
+				local pos = ply:GetShootPos()
+				local tr = util.TraceLine({
+					start = pos,
+					endpos = pos + ply:GetAimVector() * 256,
+					filter = ply
+				})
+
+				local fx = EffectData()
+				fx:SetOrigin( tr.HitPos )
+				util.Effect( "camera_flash", fx, true )
+
+			end
+
+		end
+
 	end
 
 end
 
-function SWEP:ShouldDropOnDie() return false end
+if (SERVER) then return end
 
---
--- The effect when a weapon is fired successfully
---
-function SWEP:DoShootEffect()
-
-	self:EmitSound(self.ShootSound)
-	self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
-
-	if (SERVER && !game.SinglePlayer()) then
-
-		--
-		-- Note that the flash effect is only
-		-- shown to other players!
-		--
-
-		local vPos = self.Owner:GetShootPos()
-		local vForward = self.Owner:GetAimVector()
-
-		local trace = {}
-		trace.start = vPos
-		trace.endpos = vPos + vForward * 256
-		trace.filter = self.Owner
-
-		local tr = util.TraceLine(trace)
-
-		local effectdata = EffectData()
-		effectdata:SetOrigin(tr.HitPos)
-		util.Effect("camera_flash", effectdata, true)
-
-	end
-
-end
-
-if (SERVER) then return end -- Only clientside lua after this line
-
-SWEP.WepSelectIcon = surface.GetTextureID("vgui/gmod_camera")
+SWEP.WepSelectIcon = surface.GetTextureID( "vgui/gmod_camera" )
 
 -- Don't draw the weapon info on the weapon selection thing
 function SWEP:DrawHUD() end
-function SWEP:PrintWeaponInfo(x, y, alpha) end
+function SWEP:PrintWeaponInfo() end
+
+SWEP.BlockedHUDS = {
+	["CHudWeaponSelection"] = true,
+	["CHudChat"] = true
+}
 
 function SWEP:HUDShouldDraw(name)
-
-	-- So we can change weapons
-	if (name == "CHudWeaponSelection") then return true end
-	if (name == "CHudChat") then return true end
-
-	return false
-
-end
-
-function SWEP:FreezeMovement()
-
-	-- Don't aim if we're holding the right mouse button
-	if (self.Owner:KeyDown(IN_ATTACK2) || self.Owner:KeyReleased(IN_ATTACK2)) then
+	if (self.BlockedHUDS[ name ] == true) then
 		return true
 	end
 
 	return false
-
 end
 
-function SWEP:CalcView(ply, origin, angles, fov)
+function SWEP:FreezeMovement()
+	local ply = self:GetOwner()
+	if IsValid( ply ) then
+		if ply:KeyDown( IN_ATTACK2 ) then
+			return true
+		end
 
-	if (self:GetRoll() != 0) then
+		if ply:KeyReleased( IN_ATTACK2 ) then
+			return true
+		end
+	end
+
+	return false
+end
+
+function SWEP:CalcView( ply, origin, angles, fov )
+	if (self:GetRoll() ~= 0) then
 		angles.Roll = self:GetRoll()
 	end
 
 	return origin, angles, fov
-
 end
 
 function SWEP:AdjustMouseSensitivity()
-
-	if (self.Owner:KeyDown(IN_ATTACK2)) then return 1 end
+	local ply = self:GetOwner()
+	if IsValid( ply ) then
+		if ply:KeyDown( IN_ATTACK2 ) then
+			return 1
+		end
+	end
 
 	return self:GetZoom() / 80
-
 end
